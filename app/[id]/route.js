5282616266ea91bbd25985346a5234e8f22f515e
Lavendis <wongsatorn.saehao@gmail.com>
2026-08-30 01:02:14 +0700

    EDITTT

--- app/[id]/route.js
+++ app/[id]/route.js
@@ -3,7 +3,10 @@ import { Redis } from '@upstash/redis';
 
 export const dynamic = 'force-dynamic';
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN,
+});
 
 export async function GET(request, context) {
   try {
